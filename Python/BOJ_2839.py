a3c495e836beb54b1c992f138cef7a5900932a5f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-05-07 21:47:38 +0900

    Sugar

---
+++ Python/BOJ_2839.py
@@ -0,0 +1,14 @@
+'''
+Sugar
+
+https://www.acmicpc.net/problem/2839
+'''
+
+n = int(input())
+# 2000 is a upperbound of answer
+# 5001 is a maximum of input
+save = [2000] * (5001)
+save[3], save[5] = 1, 1
+for i in range(6, n + 1):
+    save[i] = min(save[i-3], save[i-5]) + 1
+print(save[n] < 2000 and save[n] or -1)
