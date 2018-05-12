deccb04199bb9d2bab893da2eb986df8bfc36747
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-05-13 08:15:56 +0900

    Studying words in a new way

---
+++ Java/BOJ_1157_new.java
@@ -0,0 +1,32 @@
+/*
+ *  Studying words in a new way
+ *
+ *  https://www.acmicpc.net/problem/1157
+ */
+
+import java.io.IOException;
+
+class BOJ_1157_new {
+    public static void main (String [] args) throws IOException {
+        int [] count = new int [26];
+        for (int c = System.in.read(); c >= 65; c = System.in.read()) {
+            count[Character.toLowerCase(c) - 'a']++;
+        }
+        int index = 0;
+        boolean isDuplicate = false;
+        for (int max = count[0], i = 1; i < 26; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                index = i;
+                isDuplicate = false;
+            } else if (count [i] == max) {
+                isDuplicate = true;
+            }
+        }
+        if (isDuplicate) {
+            System.out.println("?");
+        } else {
+            System.out.println((char)(index + 'A'));
+        }
+    }
+}
