76ca80b0a1a06dd279ac673c0fd07bffce725596
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-06-28 05:04:55 +0900

    Sorting numbers 3 - Using Counting Sort with Backward Direction

---
+++ Java/BOJ_10989_counting_backward.java
@@ -0,0 +1,65 @@
+/*
+Sorting numbers 3
+Using Counting Sort
+With Backward Direction
+
+https://www.acmicpc.net/problem/10989
+*/
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+class BOJ_10989_counting_backward {
+    public static void main(String[] args) throws IOException {
+        new BOJ_10989_counting_backward().run();
+    }
+
+    void run() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int [] data = new int [N];
+        for (int i = 0; i < N; i++) {
+            data[i] = Integer.parseInt(br.readLine());
+        }
+        countingSort(data);
+        for (int i : data) {
+            bw.write(i + "\n");
+        }
+        bw.close();
+    }
+
+    void countingSort(int [] data) {
+        int max = 0, total = 0;
+        int [] output = new int[data.length + 1];
+
+        for (int i : data) {
+            if (i > max) {
+                max = i;
+            }
+        }
+
+        int [] count = new int [max + 1];
+        int [] count_sum = new int [max + 1];
+
+        for (int i : data) {
+            count[i]++;
+        }
+
+        count_sum[0] = count[0];
+
+        for (int i = 1; i < max + 1; i++) {
+            count_sum[i] = count_sum[i-1] + count[i];
+        }
+
+        for (int i = data.length - 1; i > -1; i--) {
+            output[count_sum[data[i]]] = data[i];
+            count_sum[data[i]]--;
+        }
+
+        System.arraycopy(output, 1, data, 0, data.length);
+    }
+}
