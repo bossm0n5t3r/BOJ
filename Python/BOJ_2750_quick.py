8b13778d51b51335a695b3bbf43d7857c3e18e7d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-07-16 18:51:01 +0900

    Sorting numbers - Using Quick Sort

---
+++ Python/BOJ_2750_quick.py
@@ -0,0 +1,40 @@
+"""
+Sorting numbers
+Using Quick Sort
+
+https://www.acmicpc.net/problem/2750
+"""
+
+
+def sol():
+    data = []
+    for _ in range(int(input())):
+        data.append(int(input()))
+    quick(data, 0, len(data) - 1)
+    return data
+
+
+def quick(data, start, end):
+    left = start
+    right = end
+    pivot = data[(left + right)//2]
+    while left <= right:
+        while data[left] < pivot:
+            left += 1
+        while data[right] > pivot:
+            right -= 1
+        if left <= right:
+            temp = data[left]
+            data[left] = data[right]
+            data[right] = temp
+            left += 1
+            right -= 1
+    if start < right:
+        quick(data, start, right)
+    if end > left:
+        quick(data, left, end)
+
+
+if __name__ == '__main__':
+    for i in sol():
+        print(i)
