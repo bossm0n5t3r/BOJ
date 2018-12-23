953faebc2bbc6e07fb7b88c2602b5029a4da93f6
bossm0n5t3r <bossm0n5t3r@gmail.com>
2018-12-23 10:38:20 +0900

    Sorting numbers - Using Insertion Sort

--- Java/BOJ_2750_insertion.java
+++ Java/BOJ_2750_insertion.java
@@ -9,7 +9,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-class BOJ_2750 {
+class BOJ_2750_insertion {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -17,7 +17,7 @@ class BOJ_2750 {
         for (int i = 0; i < N; i++) {
             data[i] = Integer.parseInt(br.readLine());
         }
-        BOJ_2750 main = new BOJ_2750();
+        BOJ_2750_insertion main = new BOJ_2750_insertion();
         main.insertionSort(data);
         for (int i : data) {
             System.out.println(i);
