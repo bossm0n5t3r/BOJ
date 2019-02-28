1fc9847b35e79456be833358a72214cff7f213fb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-02-28 23:45:58 +0900

    CMD

---
+++ Python/BOJ_1032.py
@@ -0,0 +1,23 @@
+"""
+CMD
+
+https://www.acmicpc.net/problem/1032
+"""
+
+
+def sol():
+    N = int(input())
+    file_names = []
+    for i in range(N):
+        file_names.append(input())
+    result = list(file_names[0])
+    for i in range(len(result)):
+        for names in file_names:
+            if result[i] != names[i]:
+                result[i] = "?"
+                break
+    print("".join(result))
+
+
+if __name__ == "__main__":
+    sol()
