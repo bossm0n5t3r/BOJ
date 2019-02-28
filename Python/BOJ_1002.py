699d6f328318c7ec6b70ab0409baa38f2f5693e4
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-02-28 23:43:42 +0900

    Turret

---
+++ Python/BOJ_1002.py
@@ -0,0 +1,26 @@
+"""
+Turret
+
+https://www.acmicpc.net/problem/1002
+"""
+
+
+def sol():
+    T = int(input())
+    for i in range(T):
+        x1, y1, r1, x2, y2, r2 = map(int , input().split())
+        dist = (x2 - x1) ** 2 + (y2 - y1) ** 2
+        rdiff = (r1 - r2) ** 2
+        radd = (r1 + r2) ** 2
+        if dist == 0 and r1 == r2:
+            print(-1)
+        elif dist < rdiff or dist > radd:
+            print(0)
+        elif dist == rdiff or dist == radd:
+            print(1)
+        else:
+            print(2)
+
+
+if __name__ == "__main__":
+    sol()
