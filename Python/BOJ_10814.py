046eb656041e78675bf1631454500b2e772cf394
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-03-02 01:00:39 +0900

    Sorting by age

---
+++ Python/BOJ_10814.py
@@ -0,0 +1,23 @@
+"""
+Sorting by age
+
+https://www.acmicpc.net/problem/10814
+"""
+
+
+from sys import stdin
+
+
+def sol():
+    N = int(input())
+    members = [[] for _ in range(201)]
+    for i in range(N):
+        age, name = stdin.readline().rstrip().split()
+        members[int(age)].append(age + " " + name)
+    for member in members:
+        for e in member:
+            print(e)
+
+
+if __name__ == "__main__":
+    sol()
