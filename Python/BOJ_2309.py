29337209f086023de3b9fae698515c9bacf68671
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-03-05 23:24:33 +0900

    Seven Dwarfs

---
+++ Python/BOJ_2309.py
@@ -0,0 +1,22 @@
+"""
+Seven Dwarfs
+
+https://www.acmicpc.net/problem/2309
+"""
+
+
+def sol():
+    dwarfs = [int(input()) for i in range(9)]
+    dwarfs.sort()
+    all_dwarfs = sum(dwarfs)
+    for i in range(8):
+        for j in range(i + 1, 9):
+            if all_dwarfs - dwarfs[i] - dwarfs[j] == 100:
+                for k in range(9):
+                    if k not in [i, j]:
+                        print(dwarfs[k])
+                break
+
+
+if __name__ == "__main__":
+    sol()
