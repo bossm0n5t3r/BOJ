242b42886c2c1344f47471d971ddbad491a2d7e6
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-03-13 19:08:01 +0900

    Printing stars - 8

---
+++ Python/BOJ_2445.py
@@ -0,0 +1,18 @@
+"""
+Printing stars - 8
+
+https://www.acmicpc.net/problem/2445
+"""
+
+
+def sol():
+    N = int(input())
+    for i in range(2 * N - 1):
+        if i < N:
+            print('*' * (i + 1) + ' ' * 2 * (N - 1 - i) + '*' * (i + 1))
+        else:
+            print('*' * (2 * N - 1 - i) + ' ' * 2 * (i - N + 1) + '*' * (2 * N - 1 - i))
+
+
+if __name__ == "__main__":
+    sol()
