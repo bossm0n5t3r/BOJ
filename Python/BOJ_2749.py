3c0d38d33dd68a8f94c77c7576521f611c5d409f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-03-13 19:14:32 +0900

    Fibonacci number - 3

---
+++ Python/BOJ_2749.py
@@ -0,0 +1,31 @@
+"""
+Fibonacci number - 3
+
+https://www.acmicpc.net/problem/2749
+"""
+
+
+def matrix_mul(mat1, mat2):
+    result = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                result[i][j] = (result[i][j] + mat1[i][k] * mat2[k][j]) % 1000000
+    return result
+
+
+def sol():
+    n = int(input())
+    n = n - 1
+    ans = [[1, 0], [0, 1]]
+    fibonacci_matrix = [[1, 1], [1, 0]]
+    while n > 0:
+        if n % 2 == 1:
+            ans = matrix_mul(ans, fibonacci_matrix)
+        fibonacci_matrix = matrix_mul(fibonacci_matrix, fibonacci_matrix)
+        n = n // 2
+    print(ans[0][0])
+
+
+if __name__ == "__main__":
+    sol()
