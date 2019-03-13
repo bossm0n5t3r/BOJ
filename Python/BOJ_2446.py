827fb96ee5c7baeb0ce65c098c851933de3d497a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-03-13 19:08:37 +0900

    Printing stars - 9

---
+++ Python/BOJ_2446.py
@@ -0,0 +1,20 @@
+"""
+Printing stars - 9
+
+https://www.acmicpc.net/problem/2446
+"""
+
+
+def sol():
+    N = int(input())
+    save = []
+    for i in range(N, 0, -1):
+        save.append(' ' * (N - i) + '*' * (2 * i - 1))
+    for i in range(N):
+        print(save[i])
+    for i in range(N - 2, -1, -1):
+        print(save[i])
+
+
+if __name__ == "__main__":
+    sol()
