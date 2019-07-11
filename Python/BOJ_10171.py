e2cd77bb847ac347fcc9f16feb291c61ee5ee392
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-12 04:54:15 +0900

    Cat

---
+++ Python/BOJ_10171.py
@@ -0,0 +1,15 @@
+'''
+Cat
+
+https://www.acmicpc.net/problem/10171
+'''
+
+
+def sol():
+    print("\\    /\\")
+    print(" )  ( \')")
+    print("(  /  )")
+    print(" \\(__)|")
+
+if __name__ == "__main__":
+    sol()
