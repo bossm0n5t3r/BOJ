e1aa7299ec6e774e9c5b2e454e860ea5a446496d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-12 04:21:46 +0900

    Multiplication

---
+++ Python/BOJ_1629.py
@@ -0,0 +1,25 @@
+'''
+Multiplication
+
+https://www.acmicpc.net/problem/1629
+'''
+
+
+def sol():
+    A, B, C = map(int, input().split())
+    print(power(A, B, C))
+
+
+def power(A, B, C):
+    if B == 0:
+        return 1 % C
+    if B % 2 == 0:
+        tmp = power(A, B // 2, C)
+        return tmp * tmp % C
+    else:
+        tmp = power(A, (B - 1) // 2, C)
+        return ( ( (A % C) * tmp ) * tmp ) % C
+
+
+if __name__ == "__main__":
+    sol()
