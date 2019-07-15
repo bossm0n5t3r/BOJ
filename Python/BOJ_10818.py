acdf6a6cf20a36b23416e1c960947625e1ec116c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-15 12:19:14 +0900

    Minimum, Maximum

---
+++ Python/BOJ_10818.py
@@ -0,0 +1,15 @@
+'''
+Minimum, Maximum
+
+https://www.acmicpc.net/problem/10818
+'''
+
+
+def sol():
+    input()
+    tmp_array = list(map(int, input().split()))
+    print(min(tmp_array), max(tmp_array))
+
+
+if __name__ == "__main__":
+    sol()
