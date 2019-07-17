6319f66fb587426ad69cfe45b5a1b947ec17d8f0
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-18 04:19:25 +0900

    HERMAN

---
+++ Python/BOJ_3053.py
@@ -0,0 +1,18 @@
+'''
+HERMAN
+
+https://www.acmicpc.net/problem/3053
+'''
+
+
+import math
+
+
+def sol():
+    R = int(input())
+    print(math.pi * R * R)
+    print(2 * R * R)
+
+
+if __name__ == "__main__":
+    sol()
