dca8d97d4024761657553f50ea7f890d939a8152
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-29 13:04:12 +0900

    A+B

--- 1000/Main.java
+++ 1000/Main.java
@@ -1,12 +1,6 @@
-/*
-*   A + B
-*
-*   https://www.acmicpc.net/problem/1000
-*/
+import java.util.Scanner;
 
-import java.util.*;
-
-public class BOJ_1000 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a;
--- Python/BOJ_1000.py
+++
@@ -1,8 +0,0 @@
-'''
-A + B
-
-https://www.acmicpc.net/problem/1000
-'''
-
-a, b = map(int, input().split())
-print(a+b)
