acb4b9342104b6c32cbe76ea384f0cee4f7926e2
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-29 18:58:44 +0900

    Printing N reverse

---
+++ 2742/README.md
@@ -0,0 +1,3 @@
+# Printing N reverse
+
+[https://www.acmicpc.net/problem/2742](https://www.acmicpc.net/problem/2742)
---
+++ 2742/solution.py
@@ -0,0 +1,9 @@
+def sol():
+    N = int(input())
+    while N > 0:
+        print(N)
+        N -= 1
+
+
+if __name__ == "__main__":
+    sol()
