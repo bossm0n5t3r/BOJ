66dcff0111b10c19090dc118a95a94b7660ef195
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-30 00:01:29 +0900

    Arithmetic operations

---
+++ 10869/solution.py
@@ -0,0 +1,11 @@
+def sol():
+    a, b = map(int, input().split())
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(int(a / b))
+    print(a % b)
+
+
+if __name__ == "__main__":
+    sol()
