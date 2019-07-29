c97ef947d6a725d4a405035944765e9bc0f2052d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-29 13:17:03 +0900

    A/B

--- 1008/Main.java
+++ 1008/Main.java
@@ -1,17 +1,11 @@
-/*
- * 	A / B
- * 
- * 	https://www.acmicpc.net/problem/1008
- */
+import java.util.Scanner;
 
-import java.util.*;
-
-public class BOJ_1008 {
+public class Main {
 	public static void main(String [] args) {
 		Scanner sc = new Scanner(System.in);
 		double a = sc.nextDouble();
 		double b = sc.nextDouble();
-		System.out.println(a/b);
+		System.out.println(a / b);
 		sc.close();
 	}
 }
