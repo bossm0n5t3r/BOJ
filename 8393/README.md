6ea5d4e5ff6e36dffab1b54b68a0b3da471cff2f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-29 21:50:02 +0900

    Sum

---
+++ 8393/README.md
@@ -0,0 +1,3 @@
+# Sum
+
+[https://www.acmicpc.net/problem/8393](https://www.acmicpc.net/problem/8393)
---
+++ 8393/solution.py
@@ -0,0 +1,7 @@
+def sol():
+    n = int(input())
+    print( int(n * (n + 1) / 2) )
+
+
+if __name__ == "__main__":
+    sol()
