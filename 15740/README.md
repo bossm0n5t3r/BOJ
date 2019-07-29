e41b738488ca5574974927140347ca820dcf2f3c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-30 02:41:51 +0900

    A+B - 9

---
+++ 15740/README.md
@@ -0,0 +1,3 @@
+# A+B - 9
+
+[https://www.acmicpc.net/problem/15740](https://www.acmicpc.net/problem/15740)
---
+++ 15740/solution.py
@@ -0,0 +1,7 @@
+def sol():
+    a, b = map(int, input().split())
+    print(a + b)
+
+
+if __name__ == "__main__":
+    sol()
