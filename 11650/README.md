62c94db33dd0bf01b047f56f58215cb78a3e5d9d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-30 02:18:50 +0900

    Sorting Coordinates

---
+++ 11650/README.md
@@ -0,0 +1,3 @@
+# Sorting Coordinates
+
+[https://www.acmicpc.net/problem/11650](https://www.acmicpc.net/problem/11650)
---
+++ 11650/solution.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def sol():
+    input = sys.stdin.readline
+    print = sys.stdout.write
+    N = int(input())
+    coordinates = []
+    for i in range(N):
+        coordinates.append(list(map(int, input().split())))
+    for coordinate in sorted(coordinates):
+        print("%d %d\n" % (coordinate[0], coordinate[1]))
+
+
+if __name__ == "__main__":
+    sol()
