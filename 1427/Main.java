f490546534ad94fd0f45f0d9b463227ed82bf01b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-29 14:37:39 +0900

    Sort inside

---
+++ 1427/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        new sortInside().run();
+    }
+}
+
+class sortInside {
+    void run() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String N = br.readLine();
+        char [] data = N.toCharArray();
+        Arrays.sort(data);
+        for (int i = data.length - 1; i > -1; i--) {
+            sb.append(data[i]);
+        }
+        System.out.println(sb);
+    }
+}
---
+++ 1427/README.md
@@ -0,0 +1,3 @@
+# Sort inside
+
+[https://www.acmicpc.net/problem/1427](https://www.acmicpc.net/problem/1427)
