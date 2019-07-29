cd7c2e5bd3f83d6179e5aedfcafd08494fa304d8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-29 20:45:17 +0900

    AUTORI

---
+++ 2902/README.md
@@ -0,0 +1,3 @@
+# AUTORI
+
+[https://www.acmicpc.net/problem/2902](https://www.acmicpc.net/problem/2902)
---
+++ 2902/solution.py
@@ -0,0 +1,10 @@
+def sol():
+    txt = input().split('-')
+    result = ''
+    for e in txt:
+        result += e[0]
+    print(result)
+
+
+if __name__ == "__main__":
+    sol()
