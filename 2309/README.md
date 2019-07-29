aa47b040f03f9a4ae2bac71a31d76e7b3b5df921
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-29 15:56:21 +0900

    Seven Dwarfs

---
+++ 2309/README.md
@@ -0,0 +1,3 @@
+# Seven Dwarfs
+
+[https://www.acmicpc.net/problem/2309](https://www.acmicpc.net/problem/2309)
---
+++ 2309/solution.py
@@ -0,0 +1,19 @@
+def sol():
+    dwarfs = [int(input()) for i in range(9)]
+    dwarfs.sort()
+    all_dwarfs = sum(dwarfs)
+    for i in range(8):
+        check = False
+        for j in range(i + 1, 9):
+            if all_dwarfs - dwarfs[i] - dwarfs[j] == 100:
+                for k in range(9):
+                    if k not in [i, j]:
+                        print(dwarfs[k])
+                check = True
+                break
+        if check:
+            break
+
+
+if __name__ == "__main__":
+    sol()
