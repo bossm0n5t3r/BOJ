69ae8e0901cd611e243f944f3ee51c44094c61c2
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-29 13:08:02 +0900

    A-B

--- 1001/Main.java
+++ 1001/Main.java
@@ -1,12 +1,6 @@
-/*
-*   A - B
-*
-*   https://www.acmicpc.net/problem/1001
-*/
+import java.util.Scanner;
 
-import java.util.*;
-
-public class BOJ_1001 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a;
--- Python/BOJ_1001.py
+++
@@ -1,8 +0,0 @@
-'''
-A - B
-
-https://www.acmicpc.net/problem/1001
-'''
-
-a, b = map(int, input().split())
-print(a-b)
