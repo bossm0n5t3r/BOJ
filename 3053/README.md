1fb1b85e6fb98ae6a76683cbf2798710ca195364
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-29 20:58:15 +0900

    HERMAN

---
+++ 3053/README.md
@@ -0,0 +1,3 @@
+# HERMAN
+
+[https://www.acmicpc.net/problem/3053](https://www.acmicpc.net/problem/3053)
---
+++ 3053/solution.py
@@ -0,0 +1,11 @@
+import math
+
+
+def sol():
+    R = int(input())
+    print(math.pi * R * R)
+    print(2 * R * R)
+
+
+if __name__ == "__main__":
+    sol()
