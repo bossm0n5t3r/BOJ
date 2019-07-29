eb32099f87b32d2a259979f2c944fc69255adbce
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-07-29 21:48:11 +0900

    Bulky body

---
+++ 7568/README.md
@@ -0,0 +1,3 @@
+# Bulky body
+
+[https://www.acmicpc.net/problem/7568](https://www.acmicpc.net/problem/7568)
---
+++ 7568/solution.py
@@ -0,0 +1,15 @@
+def sol():
+    N = int(input())
+    people = []
+    rank = [1] * N
+    for i in range(N):
+        people.append(list(map(int, input().split())))
+    for i in range(N):
+        for j in range(N):
+            if i != j and people[i][0] < people[j][0] and people[i][1] < people[j][1]:
+                rank[i] += 1
+    print(" ".join(map(str, rank)))
+
+
+if __name__ == "__main__":
+    sol()
