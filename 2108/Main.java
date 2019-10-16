4955ecd24b945b62e267e3d661268b033db83b89
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-10-16 18:17:13 +0900

    Statistics

--- 2108/Main.java
+++ 2108/Main.java
@@ -14,62 +14,37 @@ class statistics {
     public void run() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        ArrayList <Integer> data = new ArrayList <Integer> ();
+        ArrayList<Integer> data = new ArrayList<Integer>();
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE, sum = 0;
         for (int i = 0; i < N; i++) {
-            data.add(Integer.parseInt(br.readLine()));
+            int value = Integer.parseInt(br.readLine());
+            sum += value;
+            max = Math.max(max, value);
+            min = Math.min(min, value);
+            data.add(value);
         }
-        System.out.println(arithmeticMean(data));
-        System.out.println(median(data));
-        System.out.println(mode(data));
-        System.out.println(range(data));
-    }
-    public int arithmeticMean(ArrayList <Integer> data) {
-        int sum = 0;
-        for (int i : data) {
-            sum += i;
-        }
-        return (int) Math.ceil( (float) sum / data.size() - 0.5f);
-    }
-    public int median(ArrayList <Integer> data) {
+        int range = max - min;
+        int maxAbs = Math.max(Math.abs(max), Math.abs(min));
+        int[] count = new int[2 * maxAbs + 1];
         Collections.sort(data);
-        return data.get(data.size() / 2);
-    }
-    public int mode(ArrayList <Integer> data) {
-        int maxValue = 0, maxCount = 0, check = 0, result = 0;
-
-        // Copy ArrayList to int []
-        int [] temp = new int [data.size()];
-        for (int i = 0; i < data.size(); i++) {
-            temp[i] = data.get(i);
+        int median = data.get(data.size() / 2);
+        int maxCount = 0, check = 0, mode = 0;
+        for (int value : data) {
+            count[value + maxAbs] += 1;
+            maxCount = Math.max(maxCount, count[value + maxAbs]);
         }
-
-        // Find max value of int []
-        for (int i : temp) {
-            if (maxValue < Math.abs(i)) {
-                maxValue = Math.abs(i);
-            }
-        }
-
-        // Check the count of each element of int [] and find max count of it
-        int [] count = new int [2 * maxValue + 1];
-        for (int i : temp) {
-            count[i + maxValue]++;
-            maxCount = Math.max(maxCount, count[i + maxValue]);
-        }
-
-        // Find the mode value of int []
         for (int i = 0; i < count.length; i++) {
             if (maxCount == count[i]) {
-                result = i - maxValue;
+                mode = i - maxAbs;
                 check++;
             }
             if (check == 2) {
                 break;
             }
         }
-        return result;
-    }
-    public int range(ArrayList <Integer> data) {
-        return Collections.max(data) - Collections.min(data);
+        System.out.println((int) Math.ceil((double) sum / data.size() - 0.5f));
+        System.out.println(median);
+        System.out.println(mode);
+        System.out.println(range);
     }
 }
