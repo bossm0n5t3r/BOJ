eda6d59d217a97d91c4bee0c05d192c9db9cc1b1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-10-17 19:54:06 +0900

    미로 탐색

--- 2178/Main.java
+++ 2178/Main.java
@@ -17,10 +17,59 @@ class Main {
                 data[i][j] = Integer.parseInt(tmp[j]);
             }
         }
-        boolean[][] visited = new boolean[N][M];
-        Queue<int[]> q = new LinkedList<int[]>();
+        Bfs bfs = new Bfs(data, N, M);
+        bfs.run();
+    }
+}
+
+class Bfs {
+    private int[][] data;
+    private int N;
+    private int M;
+    private boolean[][] visited;
+    private Queue<int[]> q;
+
+    Bfs(int[][] data, int N, int M) {
+        this.data = data;
+        this.N = N;
+        this.M = M;
+        this.visited = new boolean[N][M];
+        this.q = new LinkedList<int[]>();
+    }
+
+    void run() {
         visited[0][0] = true;
-        q.add(new int[] { 0, 0 });
-        
+        q.add(new int[] { 0, 0, 1 });
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            if ((current[0] == N - 1) && (current[1] == M - 1)) {
+                System.out.println(current[2]);
+                break;
+            }
+            if ((current[0] - 1 >= 0) && (data[current[0] - 1][current[1]] == 1)) {
+                if (!visited[current[0] - 1][current[1]]) {
+                    visited[current[0] - 1][current[1]] = true;
+                    q.add(new int[] { current[0] - 1, current[1], current[2] + 1 });
+                }
+            }
+            if ((current[0] < N - 1) && (data[current[0] + 1][current[1]] == 1)) {
+                if (!visited[current[0] + 1][current[1]]) {
+                    visited[current[0] + 1][current[1]] = true;
+                    q.add(new int[] { current[0] + 1, current[1], current[2] + 1 });
+                }
+            }
+            if ((current[1] - 1 >= 0) && (data[current[0]][current[1] - 1] == 1)) {
+                if (!visited[current[0]][current[1] - 1]) {
+                    visited[current[0]][current[1] - 1] = true;
+                    q.add(new int[] { current[0], current[1] - 1, current[2] + 1 });
+                }
+            }
+            if ((current[1] < M - 1) && (data[current[0]][current[1] + 1] == 1)) {
+                if (!visited[current[0]][current[1] + 1]) {
+                    visited[current[0]][current[1] + 1] = true;
+                    q.add(new int[] { current[0], current[1] + 1, current[2] + 1 });
+                }
+            }
+        }
     }
 }
