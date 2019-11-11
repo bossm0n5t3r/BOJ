864c33020035b1c0d8303b2cc2457fde0b62adf4
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-11-11 18:47:05 +0900

    N과 M (7)

--- 15656/solution.py
+++ 15656/solution.py
@@ -1,21 +1,18 @@
 def sol():
     N, M = map(int, input().split(" "))
     data = sorted(list(map(int, input().split(" "))))
-    visited = [False] * (N + 1)
-    dfs(N, M, visited, data, 0, "")
+    dfs(N, M, data, 0, "")
 
 
-def dfs(N, M, visited, data, cnt, line):
+def dfs(N, M, data, cnt, line):
     if cnt == M:
         print(line)
         return
     for i in range(1, N + 1):
-        visited[i] = True
         if cnt == 0:
-            dfs(N, M, visited, data, cnt + 1, str(data[i - 1]))
+            dfs(N, M, data, cnt + 1, str(data[i - 1]))
         else:
-            dfs(N, M, visited, data, cnt + 1, line + " " + str(data[i - 1]))
-        visited[i] = False
+            dfs(N, M, data, cnt + 1, line + " " + str(data[i - 1]))
 
 
 if __name__ == "__main__":
