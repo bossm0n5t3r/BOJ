261cc2f0e92d6140bc38adf87313cadcd959903a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-11-11 15:46:05 +0900

    N과 M (2)

--- 15650/solution.py
+++ 15650/solution.py
@@ -2,21 +2,20 @@ def sol():
     N, M = map(int, input().split(" "))
     visited = [False] * (N + 1)
     arr = [0] * M
-    dfs(N, M, visited, arr, 0)
+    dfs(N, M, visited, arr, 0, 1)
 
 
-def dfs(N, M, visited, arr, cnt):
+def dfs(N, M, visited, arr, cnt, start):
     if cnt == M:
-        if arr == sorted(arr):
-            for i in range(M):
-                print(arr[i], end=" ")
-            print()
+        for i in range(M):
+            print(arr[i], end=" ")
+        print()
         return
-    for i in range(1, N + 1):
+    for i in range(start, N + 1):
         if not visited[i]:
             visited[i] = True
             arr[cnt] = i
-            dfs(N, M, visited, arr, cnt + 1)
+            dfs(N, M, visited, arr, cnt + 1, i + 1)
             visited[i] = False
 
 
