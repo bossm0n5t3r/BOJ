52fbbc231ae0b6cbb2f69a280f5a80923713f87c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-09 09:47:21 +0900

    A+B - 8

---
+++ 11022/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int i, T, A, B;
+  cin >> T;
+  for (i = 1; i <= T; i++)
+  {
+    cin >> A >> B;
+    cout << "Case #" << i << ": " << A << " + " << B << " = " << A + B << "\n";
+  }
+  return 0;
+}
