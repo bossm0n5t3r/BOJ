dd6a133c845d0d4edee50c4b1b83ff61ce6a780c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-11 23:08:24 +0900

    가장 큰 증가 부분 수열

--- 11055/main.cpp
+++ 11055/main.cpp
@@ -6,20 +6,20 @@ int main()
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int i, j, N, series[1000], result[1000], max = 0;
+  int i, j, N, arr[1000], dp[1000], max = 0;
   cin >> N;
   for (i = 0; i < N; i++)
-    cin >> series[i];
+    cin >> arr[i];
   for (i = 0; i < N; i++)
   {
-    result[i] = series[i];
+    dp[i] = arr[i];
     for (j = 0; j < i; j++)
     {
-      if (series[j] < series[i] && result[i] < result[j] + series[i])
-        result[i] = result[j] + series[i];
+      if (arr[j] < arr[i] && dp[i] < dp[j] + arr[i])
+        dp[i] = dp[j] + arr[i];
     }
-    if (max < result[i])
-      max = result[i];
+    if (max < dp[i])
+      max = dp[i];
   }
   cout << max << "\n";
   return 0;
