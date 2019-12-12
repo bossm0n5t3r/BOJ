7ee4638d98be2816972a52a34fa77fd4e6f94632
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-12 20:24:42 +0900

    Longest Increasing Subsequence 3

---
+++ 12738/README.md
@@ -0,0 +1,3 @@
+# Longest Increasing Subsequence 3
+
+[https://www.acmicpc.net/problem/12738](https://www.acmicpc.net/problem/12738)
---
+++ 12738/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define INF 1000000001
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int i, N, A, ans = 0;
+  vector<int> vt;
+  cin >> N;
+  vt.push_back(-INF);
+  for (i = 1; i <= N; i++)
+  {
+    cin >> A;
+    if (vt.back() < A)
+    {
+      vt.push_back(A);
+      ans++;
+    }
+    else
+    {
+      vector<int>::iterator it = lower_bound(vt.begin(), vt.end(), A);
+      *it = A;
+    }
+  }
+  cout << ans << "\n";
+  return 0;
+}
