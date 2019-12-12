783c429a72fad708cb5783dbaec69deacb779a69
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-12 11:42:41 +0900

    주사위 세개

---
+++ 2480/README.md
@@ -0,0 +1,3 @@
+# 주사위 세개
+
+[https://www.acmicpc.net/problem/2480](https://www.acmicpc.net/problem/2480)
---
+++ 2480/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int a, b, c;
+  cin >> a >> b >> c;
+  if ((a == b) && (b == c))
+    cout << 10000 + a * 1000 << "\n";
+  else if (a == b)
+    cout << 1000 + a * 100 << "\n";
+  else if (b == c)
+    cout << 1000 + b * 100 << "\n";
+  else if (c == a)
+    cout << 1000 + c * 100 << "\n";
+  else
+  {
+    if (a > b)
+      b = a;
+    if (b > c)
+      c = b;
+    cout << c * 100 << "\n";
+  }
+  return 0;
+}
