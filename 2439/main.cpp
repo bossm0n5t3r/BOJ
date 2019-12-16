7db23806109a5420ed0c58017e71ccf19ba8e14b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-16 10:22:45 +0900

    Printing stars - 2

--- 2439/main.cpp
+++ 2439/main.cpp
@@ -4,12 +4,8 @@ int main() {
   int i, j, N;
   scanf("%d", &N);
   for (i = 0; i < N; i++) {
-    for (j = 0; j < N - i - 1; j++) {
-      printf(" ");
-    }
-    for (j = 0; j <= i; j++) {
-      printf("*");
-    }
+    for (j = 0; j < N - i - 1; j++) printf(" ");
+    for (j = 0; j <= i; j++) printf("*");
     printf("\n");
   }
   return 0;
