8d96b5fa790445490a025b2818ee6a2c01f1d3a3
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-16 15:11:35 +0900

    N과 M (8)

---
+++ 15657/main.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int input[10];
+int arr[10];
+
+void dfs(int depth, int start) {
+  if (depth == M) {
+    for (int i = 0; i < M; i++) cout << arr[i] << " ";
+    cout << "\n";
+    return;
+  }
+  for (int i = start; i < N; i++) {
+    arr[depth] = input[i];
+    dfs(depth + 1, i);
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> N >> M;
+  for (int i = 0; i < N; i++) cin >> input[i];
+  sort(input, input + N);
+  for (int i = 0; i < 2 * M; i++) arr[i] = ' ';
+  dfs(0, 0);
+  return 0;
+}
