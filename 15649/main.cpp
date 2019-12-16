fff7e61fe3617ccbf890dfaf11fb7aa03eb3adc2
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-16 14:08:15 +0900

    N과 M (1)

---
+++ 15649/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+char arr[20];
+bool visited[20];
+
+void dfs(int depth) {
+  if (depth == M) {
+    cout << arr << '\n';
+    return;
+  }
+  for (int i = 0; i < N; i++) {
+    if (visited[i]) continue;
+    arr[depth * 2] = i + '1';
+    visited[i] = true;
+    dfs(depth + 1);
+    visited[i] = false;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> N >> M;
+  for (int i = 0; i < 2 * M; i++) arr[i] = ' ';
+  dfs(0);
+  return 0;
+}
