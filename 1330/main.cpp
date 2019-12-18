424f287db2bbb616027b4a4dc38ffec6de094040
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-18 23:35:00 +0900

    To compare two numbers

---
+++ 1330/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+  int A, B;
+  scanf("%d %d", &A, &B);
+  if (A > B)
+    printf(">");
+  else if (A < B)
+    printf("<");
+  else
+    printf("==");
+  return 0;
+}
