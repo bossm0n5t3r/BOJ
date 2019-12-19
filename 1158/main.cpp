7023c8b96b5ca5582c2fab95b41619adb69a8652
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-19 14:50:47 +0900

    조세퍼스 문제

--- 1158/main.cpp
+++ 1158/main.cpp
@@ -1,33 +1,20 @@
-#include <iostream>
-#include <list>
+#include <cstdio>
+#include <vector>
 
 using namespace std;
 
-void josephus(int N, int K) {
-  cout << "<";
-  list<int> survivors;
-  for (int i = 1; i <= N; i++) survivors.push_back(i);
-  list<int>::iterator kill = survivors.begin();
-  for (int i = 1; i < K; i++) kill++;
-  while (N > 1) {
-    N--;
-    cout << *kill << ", ";
-    kill = survivors.erase(kill);
-    if (kill == survivors.end()) kill = survivors.begin();
-    for (int i = 0; i < K - 1; i++) {
-      kill++;
-      if (kill == survivors.end()) kill = survivors.begin();
-    }
-    if (N <= 1) break;
-  }
-  cout << *kill << ">\n";
-}
-
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int N, K;
-  cin >> N >> K;
-  josephus(N, K);
+  int N, K, X;
+  vector<int> v;
+  scanf("%d %d", &N, &K);
+  for (int i = 1; i <= N; i++) v.push_back(i);
+  X = K - 1;
+  printf("<");
+  while (N-- > 1) {
+    printf("%d, ", *(v.begin() + X));
+    v.erase(v.begin() + X);
+    X = (X + K - 1) % N;
+  }
+  printf("%d>", *v.begin());
   return 0;
 }
