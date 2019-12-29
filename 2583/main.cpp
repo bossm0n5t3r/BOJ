5467209a7464e3721f1f6cdecb8286c8c33c5fb9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-29 21:57:50 +0900

    영역 구하기

--- 2583/main.cpp
+++ 2583/main.cpp
@@ -1,58 +1,47 @@
 #include <algorithm>
 #include <cstdio>
-#include <queue>
-#include <vector>
 
 using namespace std;
 
-int M, N, K;
-int map[101][101];
-bool visited[101][101];
+int M, N, K, cnt;
+bool map[101][101];
 int dy[] = {0, 1, 0, -1};
 int dx[] = {1, 0, -1, 0};
-vector<int> ans;
 
-void bfs(int r, int c) {
-  queue<pair<int, int>> q;
-  q.push(make_pair(r, c));
-  visited[r][c] = true;
-  int cnt = 1;
-  while (!q.empty()) {
-    int r = q.front().first;
-    int c = q.front().second;
-    q.pop();
-    for (int i = 0; i < 4; i++) {
-      int nr = r + dy[i];
-      int nc = c + dx[i];
-      if (0 > nr || nr >= M || 0 > nc || nc >= N) continue;
-      if (!visited[nr][nc] && map[nr][nc] == 0) {
-        visited[nr][nc] = true;
-        cnt++;
-        q.push(make_pair(nr, nc));
-      }
-    }
+void dfs(int r, int c) {
+  map[r][c] = true;
+  cnt++;
+  for (int i = 0; i < 4; i++) {
+    int nr = r + dy[i];
+    int nc = c + dx[i];
+    if (0 > nr || nr >= M || 0 > nc || nc >= N) continue;
+    if (!map[nr][nc]) dfs(nr, nc);
   }
-  ans.push_back(cnt);
 }
 
 int main() {
-  int x1, y1, x2, y2;
+  int ans[5000];
+  int x1, y1, x2, y2, size = 0;
   scanf("%d %d %d", &M, &N, &K);
   while (K--) {
     scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
     for (int c = x1; c < x2; c++) {
       for (int r = y1; r < y2; r++) {
-        if (map[r][c] != 1) map[r][c] = 1;
+        map[r][c] = 1;
       }
     }
   }
   for (int r = 0; r < M; r++) {
     for (int c = 0; c < N; c++) {
-      if (!visited[r][c] && map[r][c] == 0) bfs(r, c);
+      if (!map[r][c]) {
+        dfs(r, c);
+        ans[size++] = cnt;
+        cnt = 0;
+      }
     }
   }
-  sort(ans.begin(), ans.end());
-  printf("%d\n", ans.size());
-  for (int n : ans) printf("%d ", n);
+  sort(ans, ans + size);
+  printf("%d\n", size);
+  for (int i = 0; i < size; i++) printf("%d ", ans[i]);
   return 0;
 }
