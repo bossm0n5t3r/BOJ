6268d8bd7ca09a9756009590f5ecdd0ee1fe44f9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-12-29 22:08:40 +0900

    단지번호붙이기

--- 2667/main.cpp
+++ 2667/main.cpp
@@ -1,52 +1,45 @@
 #include <algorithm>
 #include <cstdio>
-#include <queue>
-#include <vector>
 
 using namespace std;
 
-char map[26][26];
-int N;
-bool visited[26][26];
-int dy[] = {0, 1, 0, -1};
-int dx[] = {1, 0, -1, 0};
-vector<int> ans;
+int N, cnt;
+int map[26][26];
+int dr[] = {0, 1, 0, -1};
+int dc[] = {1, 0, -1, 0};
 
-void bfs(int r, int c) {
-  queue<pair<int, int>> q;
-  q.push(make_pair(r, c));
-  visited[r][c] = true;
-  int cnt = 1;
-  while (!q.empty()) {
-    int y = q.front().first;
-    int x = q.front().second;
-    q.pop();
-    for (int i = 0; i < 4; i++) {
-      int ny = y + dy[i];
-      int nx = x + dx[i];
-      if (0 > ny || ny >= N || 0 > nx || nx >= N) continue;
-      if (!visited[ny][nx] && map[ny][nx] == '1') {
-        visited[ny][nx] = true;
-        cnt++;
-        q.push(make_pair(ny, nx));
-      }
-    }
+void dfs(int r, int c) {
+  map[r][c] = 0;
+  cnt++;
+  for (int i = 0; i < 4; i++) {
+    int nr = r + dr[i];
+    int nc = c + dc[i];
+    if (0 <= nr && nr < N && 0 <= nc && nc < N && map[nr][nc]) dfs(nr, nc);
   }
-  ans.push_back(cnt);
 }
 
 int main() {
+  int size = 0;
+  int ans[500];
   scanf("%d", &N);
-  for (int i = 0; i < N; i++) scanf("%s", map[i]);
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
-      if (!visited[i][j] && map[i][j] == '1') bfs(i, j);
+      scanf("%1d", &map[i][j]);
     }
   }
-  sort(ans.begin(), ans.end());
-  printf("%d\n", ans.size());
-  for (int n : ans) {
-    printf("%d\n", n);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (map[i][j]) {
+        dfs(i, j);
+        ans[size++] = cnt;
+        cnt = 0;
+      }
+    }
+  }
+  sort(ans, ans + size);
+  printf("%d\n", size);
+  for (int i = 0; i < size; i++) {
+    printf("%d\n", ans[i]);
   }
   return 0;
 }
