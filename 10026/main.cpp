673e4f41724bfc9d92c92c3c5999e9d7459f966d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-01-01 04:01:32 +0900

    적록색약

--- 10026/main.cpp
+++ 10026/main.cpp
@@ -1,57 +1,32 @@
 #include <cstdio>
-#include <queue>
 
 using namespace std;
 
-int N, cnt;
+int N;
 char grid[101][101];
 bool visited[101][101];
-int dr[] = {0, 1, 0, -1};
-int dc[] = {1, 0, -1, 0};
 
-void bfs(int r, int c, bool isColorBlindness) {
-  queue<pair<int, int>> q;
-  q.push(make_pair(r, c));
-  char color = grid[r][c];
-  cnt++;
-  while (!q.empty()) {
-    int r = q.front().first;
-    int c = q.front().second;
-    q.pop();
-    for (int i = 0; i < 4; i++) {
-      int nr = r + dr[i];
-      int nc = c + dc[i];
-      if (0 > nr || nr >= N || 0 > nc || nc >= N) continue;
-      if (isColorBlindness) {
-        if (color == 'R' || color == 'G') {
-          if (!visited[nr][nc]) {
-            if (grid[nr][nc] == 'R' || grid[nr][nc] == 'G') {
-              visited[nr][nc] = true;
-              q.push(make_pair(nr, nc));
-            }
-          }
-        } else {
-          if (!visited[nr][nc] && grid[nr][nc] == color) {
-            visited[nr][nc] = true;
-            q.push(make_pair(nr, nc));
-          }
-        }
-      } else {
-        if (!visited[nr][nc] && grid[nr][nc] == color) {
-          visited[nr][nc] = true;
-          q.push(make_pair(nr, nc));
-        }
-      }
-    }
-  }
+void dfs(int r, int c, char color) {
+  if (0 > r || r >= N || 0 > c || c >= N || visited[r][c]) return;
+  if (color == ' ' && grid[r][c] == 'B') return;
+  if (color != ' ' && grid[r][c] != color) return;
+  visited[r][c] = true;
+  dfs(r + 1, c, color);
+  dfs(r - 1, c, color);
+  dfs(r, c + 1, color);
+  dfs(r, c - 1, color);
 }
 
 int main() {
   scanf("%d", &N);
   for (int i = 0; i < N; i++) scanf("%s", grid[i]);
+  int cnt = 0;
   for (int r = 0; r < N; r++) {
     for (int c = 0; c < N; c++) {
-      if (!visited[r][c]) bfs(r, c, false);
+      if (!visited[r][c]) {
+        dfs(r, c, grid[r][c]);
+        cnt++;
+      }
     }
   }
   printf("%d ", cnt);
@@ -61,7 +36,10 @@ int main() {
   }
   for (int r = 0; r < N; r++) {
     for (int c = 0; c < N; c++) {
-      if (!visited[r][c]) bfs(r, c, true);
+      if (!visited[r][c]) {
+        dfs(r, c, grid[r][c] == 'B' ? 'B' : ' ');
+        cnt++;
+      }
     }
   }
   printf("%d", cnt);
