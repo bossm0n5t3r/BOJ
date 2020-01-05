8b40ef75bf5a8fee2dbf2445ba707037da1772c0
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-01-05 20:59:16 +0900

    다리 만들기

--- 2146/main.cpp
+++ 2146/main.cpp
@@ -1,31 +1,23 @@
-#include <iostream>
+#include <cstdio>
 #include <queue>
-#include <set>
-#include <vector>
 
 using namespace std;
 
 int N;
 int map[101][101];
-bool visited[101][101];
+int bridge[101][101];
 int dr[] = {0, 0, 1, -1};
 int dc[] = {1, -1, 0, 0};
 
-int dist(pair<int, int> p1, pair<int, int> p2) {
-  return abs(p1.first - p2.first) + abs(p1.second - p2.second) - 1;
-}
-
-int bfs() {
-  vector<set<pair<int, int>>> borders;
-  int minDist = 200;
-  // 섬 이랑 섬 경계 찾고
+// 섬 numbering
+void numbering() {
+  int islandIdx = 2;
   for (int r = 0; r < N; r++) {
     for (int c = 0; c < N; c++) {
-      if (map[r][c] && !visited[r][c]) {
-        set<pair<int, int>> s;
+      if (map[r][c] == 1) {
+        map[r][c] = islandIdx;
         queue<pair<int, int>> q;
         q.push(make_pair(r, c));
-        visited[r][c] = true;
         while (!q.empty()) {
           int r = q.front().first;
           int c = q.front().second;
@@ -34,45 +26,54 @@ int bfs() {
             int nr = r + dr[i];
             int nc = c + dc[i];
             if (0 > nr || nr >= N || 0 > nc || nc >= N) continue;
-            if (map[nr][nc] == 0) {
-              s.insert(make_pair(r, c));
-            }
-            if (map[nr][nc] && !visited[nr][nc]) {
-              visited[nr][nc] = true;
+            if (map[nr][nc] == 1) {
+              map[nr][nc] = islandIdx;
               q.push(make_pair(nr, nc));
             }
           }
         }
-        borders.push_back(s);
+        islandIdx++;
       }
     }
   }
-  // 각 섬 경계마다 가장 짧은 거리 찾고
-  while (!borders.empty()) {
-    set<pair<int, int>> s1 = borders.back();
-    for (int i = 0; i < (int)borders.size() - 1; i++) {
-      set<pair<int, int>> s2 = borders[i];
-      for (pair<int, int> p1 : s1) {
-        for (pair<int, int> p2 : s2) {
-          if (abs(p1.first - p2.first) > minDist) continue;
-          if (abs(p1.second - p2.second) > minDist) continue;
-          int distance = dist(p1, p2);
-          if (minDist > distance) minDist = distance;
-        }
+}
+
+// 다리 만들기
+void makeBridge() {
+  int min = N * N + 1;
+  queue<pair<int, int>> q;
+  for (int r = 0; r < N; r++) {
+    for (int c = 0; c < N; c++) {
+      if (map[r][c] != 0) q.push(make_pair(r, c));
+    }
+  }
+  while (!q.empty()) {
+    int r = q.front().first;
+    int c = q.front().second;
+    q.pop();
+    for (int i = 0; i < 4; i++) {
+      int nr = r + dr[i];
+      int nc = c + dc[i];
+      if (0 > nr || nr >= N || 0 > nc || nc >= N) continue;
+      if (map[nr][nc] == 0) {
+        map[nr][nc] = map[r][c];
+        bridge[nr][nc] = bridge[r][c] + 1;
+        q.push(make_pair(nr, nc));
+      } else if (map[nr][nc] != map[r][c]) {
+        int totalBridge = bridge[nr][nc] + bridge[r][c];
+        min = (min > totalBridge) ? totalBridge : min;
       }
     }
-    borders.pop_back();
   }
-  return minDist;
+  printf("%d\n", min);
 }
 
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> N;
+  scanf("%d", &N);
   for (int r = 0; r < N; r++) {
-    for (int c = 0; c < N; c++) cin >> map[r][c];
+    for (int c = 0; c < N; c++) scanf("%d", &map[r][c]);
   }
-  cout << bfs();
+  numbering();
+  makeBridge();
   return 0;
 }
