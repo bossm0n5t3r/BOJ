c1c525ecc90045c3ff9a6480f31be130c4d2b918
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-01-13 03:44:01 +0900

    말이 되고픈 원숭이

--- 1600/main.cpp
+++ 1600/main.cpp
@@ -1,80 +1,67 @@
-#include <iostream>
-#include <queue>
+#include <cstdio>
 
 using namespace std;
 
 typedef struct Monkey {
-  int r;
-  int c;
-  int dist;
-  int curK;
+  int r, c, d, k;
 } Monkey;
 
-int K;
-int W, H;
 int map[201][201];
-bool visited[201][201][31];
-int dr[] = {0, 0, 1, -1};
-int dc[] = {1, -1, 0, 0};
-int dhr[] = {-2, -1, 1, 2, 2, 1, -1, -2};
-int dhc[] = {1, 2, 2, 1, -1, -2, -2, -1};
+int visited[31][201][201];
+int mdr[] = {0, 0, 1, -1};
+int mdc[] = {1, -1, 0, 0};
+int hdr[] = {-2, -1, 1, 2, 2, 1, -1, -2};
+int hdc[] = {1, 2, 2, 1, -1, -2, -2, -1};
 
-void bfs() {
-  queue<Monkey> q;
-  q.push({0, 0, 0, K});
-  visited[0][0][K] = true;
-  while (!q.empty()) {
-    int r = q.front().r;
-    int c = q.front().c;
-    int dist = q.front().dist;
-    int curK = q.front().curK;
-    q.pop();
-    if (r == H - 1 && c == W - 1) {
-      cout << dist << "\n";
-      return;
+Monkey q[100001];
+
+int main() {
+  int K, W, H;
+  scanf("%d", &K);
+  scanf("%d %d", &W, &H);
+  for (int r = 0; r < H; r++) {
+    for (int c = 0; c < W; c++) {
+      scanf("%d", &map[r][c]);
     }
-    if (curK > 0) {
+  }
+  int s = 0, e = 0;
+  q[e++] = {0, 0, 0, 0};
+  visited[0][0][0] = 1;
+  while (s < e) {
+    Monkey cur = q[s++];
+    if (cur.r == H - 1 && cur.c == W - 1) {
+      printf("%d\n", cur.d);
+      return 0;
+    }
+    if (cur.k < K) {
       for (int i = 0; i < 8; i++) {
-        int nr = r + dhr[i];
-        int nc = c + dhc[i];
+        int nr = cur.r + hdr[i];
+        int nc = cur.c + hdc[i];
         if (0 > nr || nr >= H || 0 > nc || nc >= W) continue;
-        if (map[nr][nc] != 1 && !visited[nr][nc][curK - 1]) {
+        if (!visited[cur.k + 1][nr][nc] && !map[nr][nc]) {
           if (nr == H - 1 && nc == W - 1) {
-            cout << dist + 1 << "\n";
-            return;
+            printf("%d\n", cur.d + 1);
+            return 0;
           }
-          visited[nr][nc][curK - 1] = true;
-          q.push({nr, nc, dist + 1, curK - 1});
+          visited[cur.k + 1][nr][nc] = 1;
+          q[e++] = {nr, nc, cur.d + 1, cur.k + 1};
         }
       }
     }
     for (int i = 0; i < 4; i++) {
-      int nr = r + dr[i];
-      int nc = c + dc[i];
+      int nr = cur.r + mdr[i];
+      int nc = cur.c + mdc[i];
       if (0 > nr || nr >= H || 0 > nc || nc >= W) continue;
-      if (map[nr][nc] != 1 && !visited[nr][nc][curK]) {
+      if (!visited[cur.k][nr][nc] && !map[nr][nc]) {
         if (nr == H - 1 && nc == W - 1) {
-          cout << dist + 1 << "\n";
-          return;
+          printf("%d\n", cur.d + 1);
+          return 0;
         }
-        visited[nr][nc][curK] = true;
-        q.push({nr, nc, dist + 1, curK});
+        visited[cur.k][nr][nc] = 1;
+        q[e++] = {nr, nc, cur.d + 1, cur.k};
       }
     }
   }
-  cout << -1 << "\n";
-}
-
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> K;
-  cin >> W >> H;
-  for (int r = 0; r < H; r++) {
-    for (int c = 0; c < W; c++) {
-      cin >> map[r][c];
-    }
-  }
-  bfs();
+  printf("-1\n");
   return 0;
 }
