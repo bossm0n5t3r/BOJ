be82b2912b7ce2a8b8249d5bc2a9c2dcf364ad2d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-02-11 09:54:12 +0900

    적록색약

--- 10026/main.cpp
+++ 10026/main.cpp
@@ -45,3 +45,64 @@ int main() {
   printf("%d", cnt);
   return 0;
 }
+
+// BFS method
+
+// #include <memory.h>
+
+// #include <iostream>
+// #include <queue>
+
+// using namespace std;
+
+// int N;
+// char picture[101][101];
+// bool visited[101][101];
+// int dr[] = {0, 0, 1, -1};
+// int dc[] = {1, -1, 0, 0};
+
+// char checkColor(int r, int c, bool colorBlindness) {
+//   if (colorBlindness && picture[r][c] == 'G') return 'R';
+//   return picture[r][c];
+// }
+
+// int bfs(bool colorBlindness) {
+//   memset(visited, 0, sizeof(visited));
+//   int cnt = 0;
+//   for (int r = 0; r < N; r++) {
+//     for (int c = 0; c < N; c++) {
+//       if (!visited[r][c]) {
+//         cnt++;
+//         char basic = checkColor(r, c, colorBlindness);
+//         queue<pair<int, int>> q;
+//         q.push({r, c});
+//         visited[r][c] = true;
+//         while (!q.empty()) {
+//           int curR = q.front().first;
+//           int curC = q.front().second;
+//           q.pop();
+//           for (int i = 0; i < 4; i++) {
+//             int nr = curR + dr[i];
+//             int nc = curC + dc[i];
+//             if (0 > nr || nr >= N || 0 > nc || nc >= N) continue;
+//             if (checkColor(nr, nc, colorBlindness) == basic &&
+//                 !visited[nr][nc]) {
+//               visited[nr][nc] = true;
+//               q.push({nr, nc});
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return cnt;
+// }
+
+// int main() {
+//   ios::sync_with_stdio(0);
+//   cin.tie(0);
+//   cin >> N;
+//   for (int r = 0; r < N; r++) cin >> picture[r];
+//   cout << bfs(false) << " " << bfs(true) << "\n";
+//   return 0;
+// }
