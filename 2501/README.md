f5ab4e80c6b3ada3b347d7a737bb0777286b52d3
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-02-26 15:54:04 +0900

    약수 구하기

---
+++ 2501/README.md
@@ -0,0 +1,3 @@
+# 약수 구하기
+
+[https://www.acmicpc.net/problem/2501](https://www.acmicpc.net/problem/2501)
---
+++ 2501/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int N, K;
+  cin >> N >> K;
+  for (int i = 1; i <= N; i++) {
+    if (N % i == 0) {
+      K--;
+      if (K == 0) {
+        cout << i << "\n";
+        return 0;
+      }
+    }
+  }
+  cout << "0\n";
+  return 0;
+}
